e3fc5c1ba5ef7764547e4bca43c0fc6c94a33059
B McCool <mccoolcontact@gmail.com>
2022-10-30 00:02:40 -0300

    integrated formatting through bootstrap css

---
+++ react-portfolio/src/components/Footer.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+
+const Footer = () => {
+    return (    
+        <footer>
+            <div class="row">
+                <div class="col-10">
+                    <p class="text-end">created by B McCool</p>
+                    <p class="text-end p2">fullstack web developer</p>
+                </div>
+                <div class="col-auto d-flex justify-content-center">
+                    <i class="far fa-keyboard d-flex align-items-center"></i>
+                </div>
+            </div>
+        </footer>
+    )
+};
+
+export default Footer
---
+++ react-portfolio/src/components/Navbar.jsx
@@ -0,0 +1,37 @@
+import React from 'react'
+
+const NavBar = () => {
+    return (
+        <nav class="navbar navbar-expand-md sticky-top">
+          <div class="container-fluid">
+            <a class="navbar-brand" href="https://github.com/m-ccool">b m-ccool</a>
+
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navcol-1" aria-controls="navbarText" aria-expanded="false" aria-label="Toggle navigation">
+              <span class="navbar-toggler-icon"></span>
+            </button>
+
+            <div class="collapse navbar-collapse d-md-flex align-items-sm-center justify-content-md-end" id="navcol-1">
+              <ul class="navbar-nav">
+                <li class="nav-item d-flex justify-content-center">
+                  <a class="btn nav-btn" role="button" href="https://github.com/m-ccool">
+                    <i class="fab fa-github"></i>
+                  </a>
+                </li>
+                <li class="nav-item d-flex justify-content-end">
+                  <a class="nav-link active" aria-current="page" href="#projects">projects</a>
+                </li>
+                <li class="nav-item d-flex justify-content-end">
+                  <a class="nav-link" href="#skills">skills</a>
+                </li>
+                <li class="nav-item d-flex justify-content-end">
+                  <a class="nav-link" href="#contact">contact</a>
+                </li>
+              </ul>
+            </div>
+
+          </div>
+        </nav>
+    )
+}
+
+export default NavBar
